fe78a585b188ba00463dceb294c637ac4fb3ffae
user <user@DESKTOP-9TTOJ3M>
2024-05-29 19:23:09 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Enunciado_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxNumero = new System.Windows.Forms.TextBox();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.buttonOrdenarAsc = new System.Windows.Forms.Button();
+            this.buttonOrdenarDesc = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.listBoxNumeros = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxNumero
+            // 
+            this.textBoxNumero.Location = new System.Drawing.Point(49, 49);
+            this.textBoxNumero.Name = "textBoxNumero";
+            this.textBoxNumero.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero.TabIndex = 0;
+            this.textBoxNumero.Text = "Ingresar Numeros";
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Location = new System.Drawing.Point(49, 89);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAgregar.TabIndex = 1;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // buttonOrdenarAsc
+            // 
+            this.buttonOrdenarAsc.Location = new System.Drawing.Point(49, 132);
+            this.buttonOrdenarAsc.Name = "buttonOrdenarAsc";
+            this.buttonOrdenarAsc.Size = new System.Drawing.Size(123, 23);
+            this.buttonOrdenarAsc.TabIndex = 2;
+            this.buttonOrdenarAsc.Text = "Ordenar Ascendente";
+            this.buttonOrdenarAsc.UseVisualStyleBackColor = true;
+            this.buttonOrdenarAsc.Click += new System.EventHandler(this.buttonOrdenarAsc_Click);
+            // 
+            // buttonOrdenarDesc
+            // 
+            this.buttonOrdenarDesc.Location = new System.Drawing.Point(49, 173);
+            this.buttonOrdenarDesc.Name = "buttonOrdenarDesc";
+            this.buttonOrdenarDesc.Size = new System.Drawing.Size(123, 23);
+            this.buttonOrdenarDesc.TabIndex = 3;
+            this.buttonOrdenarDesc.Text = "Ordenar Descendente";
+            this.buttonOrdenarDesc.UseVisualStyleBackColor = true;
+            this.buttonOrdenarDesc.Click += new System.EventHandler(this.buttonOrdenarDesc_Click);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(49, 214);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuscar.TabIndex = 4;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(49, 254);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEliminar.TabIndex = 5;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // listBoxNumeros
+            // 
+            this.listBoxNumeros.FormattingEnabled = true;
+            this.listBoxNumeros.Location = new System.Drawing.Point(275, 75);
+            this.listBoxNumeros.Name = "listBoxNumeros";
+            this.listBoxNumeros.Size = new System.Drawing.Size(188, 212);
+            this.listBoxNumeros.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(374, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(487, 313);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxNumeros);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonOrdenarDesc);
+            this.Controls.Add(this.buttonOrdenarAsc);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.textBoxNumero);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxNumero;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Button buttonOrdenarAsc;
+        private System.Windows.Forms.Button buttonOrdenarDesc;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.ListBox listBoxNumeros;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enunciado_2._1
+{
+    public partial class Form1 : Form
+    {
+        private List<int> G8_numeros = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int G8_numero = int.Parse(textBoxNumero.Text);
+                G8_numeros.Add(G8_numero);
+                MostrarLista();
+                textBoxNumero.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void buttonOrdenarAsc_Click(object sender, EventArgs e)
+        {
+            G8_numeros.Sort();
+            MostrarLista();  // Actualizar la lista mostrada 
+        }
+
+        private void buttonOrdenarDesc_Click(object sender, EventArgs e)
+        {
+            G8_numeros.Sort((a, b) => b.CompareTo(a));
+            MostrarLista();  // Actualizar la lista mostrada 
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int G8_numero = int.Parse(textBoxNumero.Text);
+                int G8_indice = G8_numeros.IndexOf(G8_numero);
+                if (G8_indice != -1)
+                {
+                    MessageBox.Show($"Número encontrado en la posición: {G8_indice}");
+                }
+                else
+                {
+                    MessageBox.Show("Número no encontrado.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int G8_numero = int.Parse(textBoxNumero.Text);
+                if (G8_numeros.Remove(G8_numero))
+                {
+                    MessageBox.Show("Número eliminado.");
+                }
+                else
+                {
+                    MessageBox.Show("Número no encontrado.");
+                }
+                MostrarLista();  // Actualizar la lista mostrada
+                textBoxNumero.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private void MostrarLista()
+        {
+            listBoxNumeros.Items.Clear();  // Limpiar el ListBox
+            foreach (var num in G8_numeros)
+            {
+                listBoxNumeros.Items.Add(num);  // Agregar cada número al ListBox
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
